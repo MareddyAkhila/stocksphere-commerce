e824c4a9d73e57fa24a840d84794064743dad4f0
Mareddy Akhila <akhilamareddy21@gmail.com>
2026-05-25 23:37:17 +0530

    Connected frontend to backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api'
+  baseURL: 'https://stocksphere-backend-qsk9.onrender.com/api'
 })
 
 export default API
